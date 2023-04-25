796ecedf0bcffb13a4b41381fd06d1f50be1c8b7
ShivaReddyVanja <shivareddyvanja@gmail.com>
2023-04-25 21:45:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import Wallet from './components/Wallet';
+// import Payment from './components/Payment';
+import PaymentHandler from "./components/PaymentHandler";
+
+function App() {
+  
+
+  return (
+    <div>
+     <PaymentHandler/>
+    </div>
+  );
+}
+
+export default App;
+
