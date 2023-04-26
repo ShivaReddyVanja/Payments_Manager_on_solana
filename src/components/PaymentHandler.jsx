1ee589f962fb2f2f51ab7bf755d7408842610917
ShivaReddyVanja <114795423+ShivaReddyVanja@users.noreply.github.com>
2023-04-26 21:41:01 +0530

    i have updated the payment handler function, to send a transaction from the given secret key
    this is not the final function for handling the payments, this just send the transaction without connecting phantom wallet

--- src/components/PaymentHandler.jsx
+++ src/components/PaymentHandler.jsx
@@ -1,68 +1,48 @@
 //some pieces of code are commented beacuse they are not necessary, or causing unwanted problems
-import { useState } from "react";
-import { Buffer } from "buffer";
-import { Connection, PublicKey, SystemProgram ,Transaction} from "@solana/web3.js";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
-
-const connection = new Connection("https://api.mainnet-beta.solana.com");
-
-const amount = 1; // Change this to the desired payment amount
-const destination = "2qHKuKEevsyzqMSFLEhL69MyG4uNY8LnTJDcxVTT631J"; // Change this to the recipient's wallet address
-// const memo = "YOUR"; // Change this to a unique memo for your transaction
+import { Connection,PublicKey,Keypair,Transaction,LAMPORTS_PER_SOL,SystemProgram, sendAndConfirmTransaction} from "@solana/web3.js";
 
 export default function PaymentHandler() {
-  const [status, setStatus] = useState("Connect your Solana wallet");
-  // const { publicKey, wallet } = useWallet();
-  const [wallet, setWallet] = useState(null);
+  const secret= Uint8Array.from([142,12,245,90,104,248,153,220,17,80,189,246,246,246,183,203,65,192,216,246,126,51,17,45,168,189,196,233,35,189,83,168,187,104,149,68,181,181,221,131,183,196,79,252,166,159,31,129,21,112,94,217,144,27,74,20,125,123,67,68,84,252,206,22])
+const fromKeyPair = Keypair.fromSecretKey(secret);
+const toPublicKey = new PublicKey('2qHKuKEevsyzqMSFLEhL69MyG4uNY8LnTJDcxVTT631J');
  
-
-  function toBufferLE2(value, length) {
-    const buff = new buffer.Buffer(length);
-    buff.writeBigInt64LE(BigInt(value));
-    return buff;
-  }
 
   const connectWallet = async () => {
     const provider = new PhantomWalletAdapter();
     await provider.connect();
-    setWallet(provider);
-    setStatus(`Connected to wallet ${wallet.publicKey.toBase58()}`);
+    
+    
   };
  
-  const sendPayment = async () => {
-    // const walletAccount = await connectWallet.provider.getAccountInfo(wallet.publicKey);
+  const transferSol = async (from,to,amount)=>{
+    const connection = new Connection('https://api.devnet.solana.com','confirmed');
+    
     const transaction = new Transaction();
-    const publicKey = wallet.publicKey;
-    console.log(publicKey)
-
-    transaction.add(
-      SystemProgram.transfer({
-        fromPubkey: publicKey,
-        toPubkey: new PublicKey(destination),
-        lamports: amount * 1000000000,
-      })
-    );
-    console.log("demo")
-//try to fix this blockchash, and sign the transaction , and send it
-    const { blockhash } = await connection.getLatestBlockhash();
-    transaction.recentBlockhash = blockhash;
-
-    const signedTransaction= await wallet.signTransaction(transaction);
-
-    const signature = connection.addSignature(signedTransaction);
-
-    setStatus(`Payment sent with signature: ${signature}`);
-
-    const transactionId = await connection.sendTransaction(signedTransaction, {skipPreflight: false});
-
-    await connection.confirmTransaction(signature);
-    setStatus(`Transaction ${transactionId} sent`);
-  };
+    const instruction = SystemProgram.transfer({
+     fromPubkey:from.publicKey,
+     toPubkey:to,
+     lamports:LAMPORTS_PER_SOL * amount
+    }) ;
+    transaction.add(instruction);
+    await sendAndConfirmTransaction(connection,transaction,[
+        from
+     ])
+     console.log("done")
+ }
 
   return (
     <div>
-      <button onClick={connectWallet}>{status}</button>
-      <button onClick={sendPayment}>Send Payment</button>
+      <button onClick={connectWallet}>{}</button>
+    
+        <button onClick={(async ()=>{
+    
+ 
+    await transferSol(fromKeyPair,toPublicKey,0.2);
+   
+})}>Send Payment</button>
+    
     </div>
   );
 }
+
